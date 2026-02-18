e27989b87e63f0f5383b5aa695f8a99da2395af2
sebastian <manijustworkhere@gmail.com>
2026-02-18 14:42:25 -0500

    got an error while hosting, i think i got it

---
+++ src/app/test/selfie/page.tsx
@@ -0,0 +1,11 @@
+// src/app/test/selfie/page.tsx
+"use client";
+
+export default function SelfiePage() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Selfie Page</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
